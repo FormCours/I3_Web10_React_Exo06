94408a0462e00e9ab84c3cafc9d512d7093b2fad
Aurélien <aurelien.strimelle@bstorm.be>
2022-02-07 15:23:55 +0100

    End of demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import DateDuJour from './components/date-du-jour/date-du-jour';
+import Horloge from './components/horloge/horloge';
 
 function App() {
+
+  const [display, setDisplay] = useState(false);
+
+  const handleToggleDisplay = () => {
+    setDisplay(c => !c);
+  };
+
   return (
     <div className="App">
       <h1>Exo 06 - Le cycle de vie</h1>
+
+      <button onClick={handleToggleDisplay}>Toggle</button>
+
+      {display ? (
+        <Horloge />
+      ): (
+        <DateDuJour />
+      )}
     </div>
   );
 }
