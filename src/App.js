2dd7e727d7823ed1d28755a6a1cef3b5c3ac7946
Aurélien <aurelien.strimelle@bstorm.be>
2022-02-07 13:48:28 +0100

    Start correction

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Exo 06 - Le cycle de vie</h1>
     </div>
   );
 }
